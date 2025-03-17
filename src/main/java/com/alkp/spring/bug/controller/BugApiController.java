ede06c2a4f891b38d9bbf3b8e1dfdaac269e910d
alkp <apasenko@ya.ru>
2025-03-17 18:38:42 +0300

    add GET for Bug controller, add BugEntity & BugService

--- src/main/java/com/alkp/spring/bug/controller/BugApiController.java
+++ src/main/java/com/alkp/spring/bug/controller/BugApiController.java
@@ -1,14 +1,34 @@
 package com.alkp.spring.bug.controller;
 
+import com.alkp.spring.base.exception.ResourceNotFountException;
+import com.alkp.spring.bug.entity.BugEntity;
+import com.alkp.spring.bug.service.BugService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BugApiController {
+    private final BugService bugService;
+
+    public BugApiController(BugService bugService) {
+        this.bugService = bugService;
+    }
 
     @GetMapping("/")
     public String ok() {
         return "ok";
     }
 
+    @GetMapping("/api/v1/bug")
+    public List<BugEntity> all() {
+        return bugService.all();
+    }
+
+    @GetMapping("/api/v1/bug/{id}")
+    public BugEntity byId(@PathVariable Integer id) {
+        return bugService.byId(id).orElseThrow(ResourceNotFountException::new);
+    }
 }
---
+++ src/main/java/com/alkp/spring/bug/service/BugService.java
@@ -0,0 +1,37 @@
+package com.alkp.spring.bug.service;
+
+import com.alkp.spring.bug.entity.BugEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BugService {
+    static List<BugEntity> bugStorage = new ArrayList<>();
+
+    public BugService() {
+        fillStorage();
+    }
+
+    public void fillStorage() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            BugEntity bug = new BugEntity();
+            bug.setId(i);
+            bug.setTitle("Bug #" + random.nextInt(100, 999));
+            bug.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            bugStorage.add(bug);
+        }
+    }
+
+    public List<BugEntity> all() {
+        return bugStorage;
+    }
+
+    public Optional<BugEntity> byId(int id) {
+        return bugStorage.stream().filter(bug -> bug.getId() == id).findFirst();
+    }
+}
